71ad5e086f2397df0041b665cef3e658c94a4058
ZeroByter <romgamesrom@gmail.com>
2022-12-18 10:15:22 +0200

    changed to use code key shortcuts

--- client/tools/selectTool.ts
+++ client/tools/selectTool.ts
@@ -1,3 +1,4 @@
+import { clamp } from "@client/utils";
 import Location from "@shared/types/location";
 import Selection from "@shared/types/selection";
 import { PaintContextType } from "components/contexts/paint";
@@ -14,26 +15,37 @@ class SelectTool extends Tool {
   }
 
   onMouseDown(state: PaintContextType, args: OnClickArgs): void {
-    const { setSelection, mouseLoc, setSelectionClickability } = state;
+    const { setSelection, mouseLoc, setSelectionClickability, width, height } =
+      state;
 
     setSelection(new Selection(0, 0, 0, 0));
 
     this.dragStartLocation = mouseLoc.copy();
+    this.dragStartLocation.x = clamp(this.dragStartLocation.x, 0, width);
+    this.dragStartLocation.y = clamp(this.dragStartLocation.y, 0, height);
     setSelectionClickability(1);
   }
 
   onDrag(state: PaintContextType, args: OnDragArgs): void {
-    const { mouseLoc, setSelection } = state;
+    const { mouseLoc, setSelection, width, height } = state;
 
-    const offset = mouseLoc.minus(this.dragStartLocation);
+    const newMouseLoc = mouseLoc.copy();
+    newMouseLoc.x = clamp(newMouseLoc.x, 0, width);
+    newMouseLoc.y = clamp(newMouseLoc.y, 0, height);
+
+    const offset = newMouseLoc.minus(this.dragStartLocation);
+
+    const newWidth = Math.min(
+      this.dragStartLocation.x,
+      this.dragStartLocation.x + offset.x
+    );
+    const newHeight = Math.min(
+      this.dragStartLocation.y,
+      this.dragStartLocation.y + offset.y
+    );
 
     setSelection(
-      new Selection(
-        Math.min(this.dragStartLocation.x, this.dragStartLocation.x + offset.x),
-        Math.min(this.dragStartLocation.y, this.dragStartLocation.y + offset.y),
-        Math.abs(offset.x),
-        Math.abs(offset.y)
-      )
+      new Selection(newWidth, newHeight, Math.abs(offset.x), Math.abs(offset.y))
     );
   }
 
