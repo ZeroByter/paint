af84d90bf3505022e214b5b664b3d2b536420296
Rom Levtov <romlev@rafael.co.il>
2022-12-21 18:00:37 +0200

    added tool keybinds

--- components/paint/tools/toolsPanel.tsx
+++ components/paint/tools/toolsPanel.tsx
@@ -1,12 +1,29 @@
-import Tools from "@client/tools";
-import { FC } from "react";
+import useWindowEvent from "@client/hooks/useWindowEvent";
+import Tools, { ToolKeyShortcuts } from "@client/tools";
+import { PaintFetcher } from "components/contexts/paint";
+import { FC, useCallback } from "react";
 import ToolButton from "./toolButton";
 import css from "./toolsPanel.module.scss";
 
 const ToolsPanel: FC = () => {
+  const { setActiveToolId } = PaintFetcher();
+
   const renderTools = Object.entries(Tools).map(([id, tool]) => {
     return <ToolButton key={id} id={id} tool={tool} />;
   });
+
+  useWindowEvent(
+    "keydown",
+    useCallback(
+      (e: KeyboardEvent) => {
+        const tool = ToolKeyShortcuts[e.code];
+        if (tool) {
+          setActiveToolId(tool);
+        }
+      },
+      [setActiveToolId]
+    )
+  );
 
   return <div className={css.root}>{renderTools}</div>;
 };
